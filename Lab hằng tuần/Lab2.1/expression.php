744ca983ee8c1d67324620ee517db9c4cc7838b7
duyhacde <duyhacde@gmail.com>
2022-04-25 21:46:05 +0700

    Commit Lab2.1 version 2

---
+++ Lab hằng tuần/Lab2.1/expression.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Expression Example</title>
+    </head>
+    <body>
+        <?php 
+            $gr1 = 50;
+            $gr2 = 100;
+            $gr3 = 75;
+            $aver = ($gr1 + $gr2 + $gr3) / 3;
+            print "The average is $aver</br></br>";
+            print "https://github.com/haitranduc4270/project_web_20212"
+        ?>
+    </body>
+</html>
+
+
