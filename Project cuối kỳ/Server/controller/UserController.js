a8f1074be219ea42f2e5cc40705f8c269a7b4200
haitranduc4270 <hai.tranduc4270@gmail.com>
2022-07-15 23:07:33 +0700

    Feature Query by regular

--- Project cuối kỳ/Server/controller/UserController.js
+++ Project cuối kỳ/Server/controller/UserController.js
@@ -15,7 +15,7 @@ async function getUsers(req, res, next) {
             res.end();
         }
         else if(email) {
-            const users = await Users.find({ email: email}, { publicKey: 0 }).populate(
+            const users = await Users.find({ email: { $regex: email,  $options: 'i' }}, { publicKey: 0 }).populate(
                 {
                     path: 'cart',
                 }
@@ -67,7 +67,7 @@ async function getUsersByEmail(req, res, next) {
     try{
         const { email } = req.url.query;
         if(email){
-            const user = await Users.findOne({email: email}, { publicKey: 0 }).populate(
+            const user = await Users.findOne({email: email}, { publicKey: 0 }).populate(    // ko cho reg la vi ly do bao mat
                 {
                     path: 'cart',
                 }
