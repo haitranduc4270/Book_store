201308e64d951a396e72144e71c7c9febc5ac97e
haitranduc4270 <hai.tranduc4270@gmail.com>
2022-07-19 15:55:51 +0700

    Fix Protocol POST -> GET

--- Project cuối kỳ/Server/router/LoginRouter.js
+++ Project cuối kỳ/Server/router/LoginRouter.js
@@ -1,10 +1,12 @@
 const Route = require('../library/router');
 const loginController = require('../controller/LoginCotroller.js');
+const { verify } = require('../service/authenticate.js');
 
 const route = new Route();
 
 route.use('/api/v1/login/user', 'post', loginController.loginUser);
 route.use('/api/v1/login/admin', 'post', loginController.loginAdmin);
+route.use('/api/v1/logout', 'get', verify, loginController.logout);
 
 
 module.exports = route;
